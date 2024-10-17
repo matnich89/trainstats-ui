62727e349a3fe87599de46f1492ca4fdebc10fe7
mathewnicholls <mathewnicholls@protonmail.com>
2024-10-17 12:19:35 +0100

    hardcode protocol

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
 	// Construct the WebSocket URL based on the current request URL
-	const wsProtocol = url.protocol === 'https:' ? 'wss:' : 'ws:';
-	const wsUrl = `${wsProtocol}//${url.host}/api/ws`;
+	const wsUrl = `ws://${url.host}/api/ws`;
 
 	return {
 		wsUrl
