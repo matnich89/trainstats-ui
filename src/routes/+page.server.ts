f8d299b017ec765a93840b5275cf638e1bf01330
mathewnicholls <mathewnicholls@protonmail.com>
2024-10-18 08:36:25 +0100

    fix protocol

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ export const load: PageServerLoad = async ({ url }) => {
 	// Use localhost and port 8080 for local development
 	const wsHost = process.env.NODE_ENV === 'development' ? 'localhost:8080' : url.host;
 
-	const wsUrl = `ws://${wsHost}/national`;
+	const wsUrl = `wss://${wsHost}/national`;
 
 	return {
 		wsUrl
